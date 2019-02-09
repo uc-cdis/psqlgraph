71646aa14d355690803e6972cafc680c964dccc2
Zac Flamig <zac@weatherwary.com>
2019-02-08 20:57:19 -0600

    Update base.py

--- psqlgraph/base.py
+++ psqlgraph/base.py
@@ -164,14 +164,14 @@ class CommonBase(object):
         """Returns a list of hybrid_properties defined on the subclass model
 
         """
-        if not _properties_list_cache:
-            _properties_list_cache = [
+        if not cls._properties_list_cache:
+            cls._properties_list_cache = [
                 attr for attr in dir(cls)
                 if attr in cls.__dict__
                 and isinstance(cls.__dict__[attr], hybrid_property)
                 and getattr(getattr(cls, attr), '_is_pg_property', True)
             ]
-        return _properties_list_cache
+        return cls._properties_list_cache
 
     @classmethod
     def has_property(cls, key):
