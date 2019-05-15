789a398a30f32356e600ed69f30d4cdd57bca86d
Zoe Chitty <19900057+vpsx@users.noreply.github.com>
2019-05-15 17:02:10 -0500

    move __declare_last__ to new mixin class

--- psqlgraph/node.py
+++ psqlgraph/node.py
@@ -17,8 +17,43 @@ def reverse_lookup(dictionary, search_val):
         if val == search_val:
             yield key
 
+class LinkEdgeMixin:
+    @classmethod
+    def __declare_last__(cls):
+        src_ids, dst_ids = [], []
+        for scls in Edge.get_subclasses():
+            name = scls.__name__
+            name_in = '_{}_in'.format(name)
+            name_out = '_{}_out'.format(name)
+            src_assoc = getattr(scls, SRC_DST_ASSOC)
+            dst_assoc = getattr(scls, DST_SRC_ASSOC)
+            if scls.__dst_class__ == cls.__name__:
+                if not hasattr(cls, name_in):
+                    edge_in = relationship(
+                        name,
+                        foreign_keys=[scls.dst_id],
+                        backref='dst',
+                        cascade='all, delete, delete-orphan',
+                    )
+                    setattr(cls, name_in, edge_in)
+                    cls._edges_in.append(name_in)
+                    dst_ids.append(scls.dst_id)
+                cls._set_association_proxy(scls, dst_assoc, name_in, 'src')
+            if scls.__src_class__ == cls.__name__:
+                if not hasattr(cls, name_out):
+                    edge_out = relationship(
+                        name,
+                        foreign_keys=[scls.src_id],
+                        backref='src',
+                        cascade='all, delete, delete-orphan',
+                    )
+                    setattr(cls, name_out, edge_out)
+                    cls._edges_out.append(name_out)
+                    src_ids.append(scls.src_id)
+                cls._set_association_proxy(scls, src_assoc, name_out, 'dst')
 
-class Node(AbstractConcreteBase, ORMBase):
+
+class Node(AbstractConcreteBase, ORMBase, LinkEdgeMixin):
 
     @declared_attr
     def _edges_out(self):
@@ -63,39 +98,6 @@ class Node(AbstractConcreteBase, ORMBase):
     def edges_out(self):
         return [e for rel in self._edges_out for e in getattr(self, rel)]
 
-    @classmethod
-    def __declare_last__(cls):
-        src_ids, dst_ids = [], []
-        for scls in Edge.get_subclasses():
-            name = scls.__name__
-            name_in = '_{}_in'.format(name)
-            name_out = '_{}_out'.format(name)
-            src_assoc = getattr(scls, SRC_DST_ASSOC)
-            dst_assoc = getattr(scls, DST_SRC_ASSOC)
-            if scls.__dst_class__ == cls.__name__:
-                if not hasattr(cls, name_in):
-                    edge_in = relationship(
-                        name,
-                        foreign_keys=[scls.dst_id],
-                        backref='dst',
-                        cascade='all, delete, delete-orphan',
-                    )
-                    setattr(cls, name_in, edge_in)
-                    cls._edges_in.append(name_in)
-                    dst_ids.append(scls.dst_id)
-                cls._set_association_proxy(scls, dst_assoc, name_in, 'src')
-            if scls.__src_class__ == cls.__name__:
-                if not hasattr(cls, name_out):
-                    edge_out = relationship(
-                        name,
-                        foreign_keys=[scls.src_id],
-                        backref='src',
-                        cascade='all, delete, delete-orphan',
-                    )
-                    setattr(cls, name_out, edge_out)
-                    cls._edges_out.append(name_out)
-                    src_ids.append(scls.src_id)
-                cls._set_association_proxy(scls, src_assoc, name_out, 'dst')
 
     @classmethod
     def _set_association_proxy(cls, edge_cls, attr_name, edge_name, direction):
