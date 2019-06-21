c602da92d017da19cdaeb9027afa85f089fca140
Zoe Chitty <19900057+vpsx@users.noreply.github.com>
2019-06-21 11:29:12 -0500

    chore(travis): release to pypi, auto release notes

--- setup.py
+++ setup.py
@@ -1,16 +1,31 @@
+from subprocess import check_output
 from setuptools import setup
 
+def get_version():
+    # https://github.com/uc-cdis/dictionaryutils/pull/37#discussion_r257898408
+    try:
+        tag = check_output(
+            ["git", "describe", "--tags", "--abbrev=0", "--match=[0-9]*"]
+        )
+        return tag.decode("utf-8").strip("\n")
+    except Exception:
+        raise RuntimeError(
+            "The version number cannot be extracted from git tag in this source "
+            "distribution; please either download the source from PyPI, or check out "
+            "from GitHub and make sure that the git CLI is available."
+        )
+
 setup(
-    version='2.0.1',
     name='psqlgraph',
+    version=get_version(),
     packages=["psqlgraph"],
     install_requires=[
-        'psycopg2==2.7.3.2',
+        'psycopg2~=2.7',
         'sqlalchemy~=1.3',
-        'py2neo==2.0.1',
+        'py2neo~=2.0',
         'progressbar',
-        'avro==1.7.7',
-        'xlocal==0.5',
-        'requests>=2.5.2, <=2.7.0'
+        'avro~=1.7',
+        'xlocal~=0.5',
+        'requests~=2.5'
     ]
 )
